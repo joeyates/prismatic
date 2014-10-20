a28244a3c5139f31a2cdf42bd827eab47f074fe7
Joe Yates <joe.g.yates@gmail.com>
2014-10-20 15:56:19 +0200

    Get local coverage reports as well [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,18 @@
 require 'rspec'
 require 'codeclimate-test-reporter'
 
-CodeClimate::TestReporter.start
-
 spec_path = File.dirname(__FILE__)
 $LOAD_PATH << File.expand_path('../lib', spec_path)
+
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_filter '/vendor/'
+  end
+end
 
 require 'prismatic'
